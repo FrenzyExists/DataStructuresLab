a15ddd49ddc489ca8fbd3226ecea6f29a2cc8b01
FrenzyExists <frenzyexists@gmail.com>
2021-03-01 13:03:11 -0400

    Try it Try it !

---
+++ Lab4/src/sorterClasses/Entero.java
@@ -0,0 +1,23 @@
+package sorterClasses;
+
+// runtime != time complexity
+// runningtime, from the worst insertion sort is the best
+
+public class Entero implements Comparable<Entero> {
+    private int value;
+    public Entero(int v) {
+        value = v;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String toString() {
+        return value + "";
+    }
+
+    public int compareTo(Entero other) {
+        if (other == null)
+            throw new IllegalArgumentException("...");
+        return this.value - other.value;           }       }
