446bd4e29dc30b1417aa91a77e93929a13b579d5
FrenzyExists <frenzyexists@gmail.com>
2021-03-15 15:28:44 -0400

    Documented Stack Interfase

--- Lab6/src/Practice/interfase/Stack.java
+++ Lab6/src/Practice/interfase/Stack.java
@@ -8,13 +8,14 @@ public interface Stack<T> {
 
     /**
      * Pushes an item onto the top of this stack.
-     * @param node
+     * @param node - The node to be pushed onto this stack
      */
     void push(T node);
 
     /**
-     *
-     * @return
+     * Removes the object at the top of this stack and returns that object as the
+     * value of this function.
+     * @return The object at the top of this stack
      */
     T pop();
 
@@ -30,8 +31,18 @@ public interface Stack<T> {
      */
     Integer size();
 
+    /**
+     * Returns the position of the node on this stack. The top of the stack is 1,
+     * while the bottom is size()
+     * @param node - the desired node.
+     * @return the position of the desired node
+     */
     Integer search(T node);
 
+    /**
+     * Is this stack empty?
+     * @return true -> Yup is empty | false -> we have nodes over here!
+     */
     boolean empty();
 
 }
